50c5a3ecfff5aa1e03976bae43db5d1c89540f47
T0M4S4S <168530649+T0M4S4S@users.noreply.github.com>
2024-12-03 12:07:49 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,15 +1,22 @@
-// script.js
 const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
 themeToggle.addEventListener('click', () => {
-
     body.classList.toggle('dark-theme');
     body.classList.toggle('light-theme');
-    
-    if (body.classList.contains('dark-theme')) {
-        themeToggle.textContent = 'Sviesus';
+
+    themeToggle.textContent = body.classList.contains('dark-theme') ? 'Light Theme' : 'Dark Theme';
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = 'block';
     } else {
-        themeToggle.textContent = 'Tamsus';
+        scrollToTopBtn.style.display = 'none';
     }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
