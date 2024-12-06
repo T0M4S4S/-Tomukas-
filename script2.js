dd0c59ca3a049d3ade60fbd193797e476c29e996
T0M4S4S <168530649+T0M4S4S@users.noreply.github.com>
2024-12-06 12:51:08 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,6 +2,19 @@ const themeToggle = document.getElementById('themeToggle');
 const body = document.body;
 const scrollToTopBtn = document.getElementById('scrollToTopBtn');
 
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
+
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
     body.classList.toggle('light-theme');
